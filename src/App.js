d3900ad8c685a860511c7f20992bf70d65e305ae
Elaine Ly <elainely2003@gmail.com>
2021-08-02 14:22:12 -0400

    Changed App.js to class based component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello from React</h1>
-    </div>
-  );
+// class based component
+class App extends React.Component {
+  render() {
+    // lifecycle method
+    return (
+      <div className="App">
+        <h1>Hello from React</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
