fbadd736472e8fcd2cb8eee126c5ab58b3b3a012
Elaine Ly <elainely2003@gmail.com>
2021-08-02 15:43:38 -0400

    Learned how to use reactJS with and without JSX

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 // class based component
-class App extends React.Component {
+class App extends Component {
   render() {
-    // lifecycle method
+    // this gives us same output without jsx
+    // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Hello from React'));
+    // use react.fragment whenever you don't have parent element 
     return (
-      <div className="App">
+      // this looks like html but is actually jsx 
+      // everything has to be enclosed within a parent element
+      <div className='App'>
         <h1>Hello from React</h1>
-      </div>
+      </div> 
     );
   }
 }
