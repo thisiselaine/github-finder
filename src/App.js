788dd49af3bc01a6643e414ade46306189a662cd
Elaine Ly <elainely2003@gmail.com>
2021-08-03 00:03:45 -0400

    Worked with props and prop types in navbar and main app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
+import { Navbar } from './components/layout/Navbar';
 import './App.css';
 
 // class based component
 class App extends Component {
   render() {
-    const name = 'John Doe';
-    const loading = false;
-    const showName = false; 
-
     return (
       <div className='App'>
-        <h1>My App</h1>
-        { loading ? <h4> Loading... </h4> : <h1>Hello {showName && name}</h1>}
+        <Navbar />
       </div> 
     );
   }
