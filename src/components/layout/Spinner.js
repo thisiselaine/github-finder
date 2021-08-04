aecf7307e59216e2bef7d23c67d6c9cf1299ae71
Elaine Ly <elainely2003@gmail.com>
2021-08-04 13:51:02 -0400

    registered oauth application to get unlimited requests

--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -1,12 +1,14 @@
 import React, { Fragment } from 'react';
 import spinner from './spinner.gif';
 
-export const Spinner = () => {
+const Spinner = () => (
   <Fragment>
     <img
       src={spinner}
       alt='Loading...'
       style={{ width: '200px', margin: 'auto', display: 'block' }}
     ></img>
-  </Fragment>;
-};
+  </Fragment>
+);
+
+export default Spinner;
