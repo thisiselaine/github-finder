7cc8f2e4809adeef1e3f449728e8cb43b5e5ab8b
Elaine Ly <elainely2003@gmail.com>
2021-08-04 15:40:44 -0400

    Added an about page, used react router dom for links on navbar

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>About Github Finder</h1>
+      <p>This app is used to search Github users.</p>
+      <p>Version: 1.0.0</p>
+    </Fragment>
+  );
+};
+
+export default About;
