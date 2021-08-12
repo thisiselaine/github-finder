76b016991a53e5929bc484c2a0af5f22d33dee4b
Elaine Ly <elainely2003@gmail.com>
2021-08-12 14:28:30 -0400

    Fixed rendering error

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ class App extends Component {
     this.setState({ loading: true });
 
     const res = await axios.get(
-      `https://api.github.com/users/${username}&client_id
+      `https://api.github.com/users/${username}?client_id
       =${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
     this.setState({ user: res.data, loading: false });
